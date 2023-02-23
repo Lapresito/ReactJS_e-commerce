3b21c46808e3c2e8a9de85a7a725875d95172e21
Joaquin Laprovitera <joacolaprovitera@gmail.com>
2023-02-22 21:29:33 -0300

    2da Pre-Entrega Proyecto Final ReactJS + GIF & Alert

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Bienvenidos a Unniversse E-Commerce
 
+Esta es una pagina que simula un E-Commerce desde la parte Front-End.
+Se trata de un E-Shop sobre cuerpos en el Universo, algo irracional pero llamativo para una prueba de comercio, generado con las herramientas React JavaScript en formato JSX, routes y vite.
+Tambien incluye estilos proporcionados por Chakra ui.
+Cuenta con una pagina de inicio que te da una bienvenida, un catalogo con todos los objetos a vender y un Menu desplegable que te ayuda a filtrar por categoria.
+Cada objeto tiene sus respectivos detalles para que sepas que estas comprando! Simplemente seleccionaras los que quieras comprar siendo el maximo esl stock disponible, luego los puedes agregar al Carrito.
+Tambien cuenta con un carrito al cual puedes acceder en todo momento para llevar una visualizacion adecuada de lo que vas a querer comprar y un formulario para que se te envie tu compra y guardar clientes.
 ---
 ### Recomendaciones:
 
@@ -21,12 +27,12 @@ Esperar el tiempo de carga y luego realizar CTRL + Click en el link brindado por
 ---
 >Aqui se puede ver un GIF que muestra rapidamente el flujo y contenido de la pagina.
 
-![GIF](url)
+![Muestra del sitio](./src/assets/UnniversseEshop.gif)
 ---
 #### Datos del creador
 |Nombre|Apellido|Mail|
 |---|---|---|
 |Joaquin|Laprovitera|joacolaprovitera@gmail.com|
 
-Academia donde estudio: 
+Academia de estudio: 
 [CODERHOUSE](https://plataforma.coderhouse.com/cursos)
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,34 +1,55 @@
 import React from "react";
-import { Button, Divider, Text, Center } from "@chakra-ui/react";
+import { Button, Divider, Text, Center, Alert, AlertIcon } from "@chakra-ui/react";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
 
 const ItemCount = ({ top }) => {
   const [count, setCount] = useState(1);
+  const [showAlert, setShowAlert] = useState(false);
+
   const addItem = () => {
-    if (count != top) {
+    if (count < top) {
       setCount(count + 1);
     } else {
-      console.log("error");
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 3000);
     }
   };
+
   const substractItem = () => {
-    if (count != 1) {
+    if (count > 1) {
       setCount(count - 1);
     }
   };
+
   const sendToCart = () => {
     setCount(1);
   };
+
   return (
     <>
       <Center>
-        <Button onClick={() => substractItem()}> - </Button>
-        <Text fontSize='1.5rem' p='2px'> {count} </Text>
-        <Button onClick={() => addItem()}>+</Button>
-        <Divider/>
-        <Button width='100%' onClick={sendToCart}>Add to Cart</Button>
+        <Button onClick={substractItem}> - </Button>
+        <Text fontSize='1.5rem' p='2px'>
+          {count}
+        </Text>
+        <Button onClick={addItem}>+</Button>
+        <Divider />
+        {top > 0 ? (
+          <Button width='100%' onClick={sendToCart}>
+            Add to Cart
+          </Button>
+        ) : (
+          <Text color='gray.500'>Out of stock</Text>
+        )}
       </Center>
+      {showAlert && (
+        <Alert status='warning'>
+          <AlertIcon />
+          No more stock
+        </Alert>
+      )}
     </>
   );
 };
